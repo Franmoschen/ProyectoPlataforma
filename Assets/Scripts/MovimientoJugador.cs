5548cf1f5cc1c94c25e777516385ee1a7bc9b246
Franmoschen <71043365+Franmoschen@users.noreply.github.com>
2021-04-28 21:04:37 -0300

    Fisrt commit

---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public float Velocidad = 2;
+    public float Salto = 3;
+    Rigidbody2D rb;
+    public bool SaltoMejorado= false;
+    public float caida= 0.5f;
+    public float Bajo = 1f;
+    public SpriteRenderer spriteRenderer;
+    public Animator Animacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+   void FixedUpdate()
+    {
+
+        if (Input.GetKey("d")||(Input.GetKey("right")))
+        {
+            rb.velocity = new Vector2(Velocidad, rb.velocity.y);
+            spriteRenderer.flipX = false;
+            Animacion.SetBool("Correr", true);
+        }
+        else if (Input.GetKey("a") || (Input.GetKey("left")))
+        {
+            rb.velocity = new Vector2(-Velocidad, rb.velocity.y);
+            spriteRenderer.flipX = true;
+           Animacion.SetBool("Correr", true);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            Animacion.SetBool("Correr", false);
+        }
+        if (Input.GetKey("w")||(Input.GetKey("up"))&&(CheckGround.IsGrounded))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Salto);
+        }
+        if (CheckGround.IsGrounded==false)
+        {
+            Animacion.SetBool("Saltar", true);
+            Animacion.SetBool("Correr", false);
+        }
+        if (CheckGround.IsGrounded==true)
+        {
+            Animacion.SetBool("Saltar", false);
+            
+        }
+        if (SaltoMejorado)
+        {
+            if (rb.velocity.y<0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (caida) * Time.deltaTime;
+            }
+            if (rb.velocity.y>0 && !Input.GetKey("w"))
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (Bajo) * Time.deltaTime;
+            }
+        }
+    }
+}
